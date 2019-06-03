bb44adb5870c61191c8d19758fbd80348ae12e06
Leandro Andres Dalmasso <leandrodalmasso@gmail.com>
2019-06-03 16:18:16 -0300

    [redux] Update counter-vanilla

--- redux/counter-vanilla/main.js
+++ redux/counter-vanilla/main.js
@@ -1,32 +1,39 @@
-// Action types
+/*
+Principles of redux:
 
+1. The whole state of the app is represented as a single JS object.
+
+2. State is read-only.
+The only way to change the state is to dispatch an action.
+An action is a JS object that describes what happened.
+
+3. Changes are made with pure functions.
+To specify how the state tree is transformed by actions, you write reducers.
+Reducers are pure functions that take the previous state and an action and return the new state.
+
+-----------------------
+
+The store:
+
+- Holds the app's state object
+- Allows access to state via getState()
+- Lets you dispatch actions via dispatch(action)
+- When an action is dispatched, it calls the reducer with the state and the action to get a new state.
+- Registers listeners via subscribe(listener). Once the state is updated the listeners execute to update the UI.
+*/
+
+// action types
 const ADD = 'ADD';
 const MINUS = 'MINUS';
 const RESET = 'RESET';
 
-// Action creators
+// action creators
+const add = () => ({ type: ADD });
+const minus = () => ({ type: MINUS });
+const reset = () => ({ type: RESET });
 
-function add() {
-  return {
-    type: ADD,
-  };
-}
-
-function minus() {
-  return {
-    type: MINUS,
-  };
-}
-
-function reset() {
-  return {
-    type: RESET,
-  }
-}
-
-// Reducer
-
-function reducer(state = { count: 0, }, action) {
+// reducer
+const reducer = (state = { count: 0, }, action) => {
   switch (action.type) {
     case ADD:
       return {
@@ -48,37 +55,34 @@ function reducer(state = { count: 0, }, action) {
   }
 }
 
-// Store
+// store
+const store = Redux.createStore(reducer);
 
-let store = Redux.createStore(reducer);
-
-// Event handlers that dispatch actions
-
+// event handlers
 const addButton = document.getElementById('add');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+
 addButton.addEventListener(
   'click',
   () => store.dispatch(add())
 );
 
-const minusButton = document.getElementById('minus');
 minusButton.addEventListener(
   'click',
   () => store.dispatch(minus())
 );
 
-const resetButton = document.getElementById('reset');
 resetButton.addEventListener(
   'click',
   () => store.dispatch(reset())
 );
 
-// Listeners
-
+// listeners
 const counterEl = document.getElementById('counter');
-let state;
 
-function render() {
-  state = store.getState();
+const render = () => {
+  const state = store.getState();
   counterEl.innerHTML = state.count.toString();
 }
 
@@ -87,21 +91,14 @@ store.subscribe(render);
 render();
 
 /*
-Data Flow
+What's happening?
 
 1. When the script is executed the store is created.
-
-2. The store calls the reducer and the reducer initializes the state.
-
+2. The store calls the reducer with a dummy action and the reducer initializes the state.
 3. The render method is executed and the counter element renders 0
-
 4. When a button is clicked an action is dispatched.
-
 5. The store calls the reducer function, passing it the current state tree and the action that was dispatched.
-
-6. The Reducer returns a new state tree.
-
+6. The reducer returns a new state tree.
 7. The store saves the complete state tree returned by the reducer.
-
 8. Listeners registered with store.subscribe(listener) are invoked, they call store.getState() to get the new state and update the UI.
 */
