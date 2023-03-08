89fcf586f80b056e7a3dfc41908a0938c6ee7972
leandrodalmasso <leandrodalmasso@gmail.com>
2023-03-08 15:50:58 -0300

    TS

--- typescript.ts
+++ typescript.ts
@@ -1,2 +1,10 @@
+// Generics
+
 type Pair<U, V> = [U, V];
 let pair: Pair<number, string> = [3, "hi"];
+
+// Function type expressions
+
+type GetProduct = (name: string) => { id: number; name: string };
+
+const getProduct: GetProduct = (name) => ({ id: 123, name });
